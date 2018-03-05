78b1ec0e462001f781efd95a4f63b42818015ecb
sunilmbhoi <sunilmbhoi@gmail.com>
2018-03-05 19:09:49 +0530

    test 1

--- lauch_container.sh
+++ lauch_container.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo docker pull 192.168.56.160:5000/nodejscustom:latest
+sudo docker pull 192.168.56.160:5000/mongo:latest
 if [ ! -d /home/pramati/nodejs ] ; then
  mkdir -p /home/pramati/nodejs
 fi
