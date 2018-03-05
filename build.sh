7424c063d76fb70cf89a711b9b222beeadccbbfb
sunilmbhoi <sunilmbhoi@gmail.com>
2018-03-05 18:04:19 +0530

    new script

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 sudo docker build -t nodejscustom:latest -f Dockerfile.nodjs .
+sudo docker pull mongo:latest
--- lauch_container.sh
+++ lauch_container.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ ! -d /home/pramati/nodejs ] ; then
+ mkdir -p /home/pramati/nodejs
+fi
+cp -av /tmp/docker-compose.yml /home/pramati/nodejs/
+cd /home/pramati/nodejs/
 sudo docker-compose stop 
 sudo docker-compose rm -f 
 sudo docker-compose up -d
